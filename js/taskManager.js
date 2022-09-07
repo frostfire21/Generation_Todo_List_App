84a4a8e920302caf705c7745645e41325600fe21
meronzenebe1 <meronzenebe1@gmail.com>
2022-09-07 12:34:30 -0400

    Add Marked as done button

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,13 +1,13 @@
-function render (taskManager) {
+function render(taskManager) {
     let taskHtmlList = []
     taskManager.tasks.array.forEach(element => {
-      let renderedTask = createTaskHtml (element.taskName, element.description, element.assignTo, dueDate, element.status)
-       taskHtmlList.push (renderedTask)
+        let renderedTask = createTaskHtml(element.taskName, element.description, element.assignTo, dueDate, element.status)
+        taskHtmlList.push(renderedTask)
     });
 }
 
 
-function createTaskHtml (name, description, assignedTo, dueDate='tbd', status) {
+function createTaskHtml(name, description, assignedTo, dueDate = 'tbd', status) {
     const htmlListItem = ` <a href="#" class="list-group-item list-group-item-action">
     <div class="card" style="width: 18rem;">
         <div class="card-body">
@@ -16,30 +16,31 @@ function createTaskHtml (name, description, assignedTo, dueDate='tbd', status) {
             <h6 class="card-subtitle mb-2 text-muted">${dueDate}</h6>
             <p class="card-text">${status}</p>
             <span class="badge text-bg-primary">In progress</span>
+            <button type="button" class= "done-button" class="btn btn-success">Mark As Done</button>
             <button type="button" class="btn btn-danger">Delete</button>
         </div>
     </div>
 </a>`
-return htmlListItem
-} 
+    return htmlListItem
+}
 
 
-class TaskManager{
-    constructor(currentId = 0){
+class TaskManager {
+    constructor(currentId = 0) {
         this.currentId = currentId;
 
         this.tasks = [];
     }
-    addTask (taskName, description, assignedTo, dueDate, status='TODO') {
+    addTask(taskName, description, assignedTo, dueDate, status = 'TODO') {
         this.currentId++;
-        
+
         let task = {
             id: this.currentId,
-            taskName : taskName,
-            description : description,
-            assignedTo : assignedTo,
-            dueDate : dueDate,
-            status :'TODO'
+            taskName: taskName,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: 'TODO'
         }
 
         this.tasks.push(task)
